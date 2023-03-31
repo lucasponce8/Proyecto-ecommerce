2747377eb0f1bc84c03591dccbdc09079c97a8b3
lucasponce8 <lucasponce8.lp@gmail.com>
2023-03-30 21:53:38 -0300

    creando ruta POST

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const { Product } = require("../models/Products");
 const router = Router();
 
-router.post('/productos', async (req, res) => {
+router.post('/product', async (req, res) => {
     const { 
         name ,
         description,
