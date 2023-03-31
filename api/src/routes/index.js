34255b446ef6589af3fd65c61e4ed3ddedaf74cd
Agustin Fraile <agustinfraile98@gmail.com>
2023-03-31 10:47:55 -0300

    agregando ruta put para modificar los productos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,50 +1,60 @@
-
 const { Router } = require("express");
 const express = require("express");
 const { Product } = require("../models/Products");
 const router = Router();
 
-router.post('/product', async (req, res) => {
-    const { 
-        name ,
-        description,
-        price,
-        image,
-        category,
-        subcategory,
-    } = req.body;
+// ruta para postear productos
+router.post("/product", async (req, res) => {
+    const { name, description, price, image, category, subcategory } = req.body;
 
-    if(!name || !description || !price || !category) {
-        res.status(404).send({message: "Error, no se puede crear el producto porque faltan datos"});
+    if (!name || !description || !price || !category) {
+        res.status(404).send({
+            message: "Error, no se puede crear el producto porque faltan datos",
+        });
     }
 
     try {
         let product = new Product({
-        name ,
-        description,
-        price,
-        image,
-        category,
-        subcategory,
+            name,
+            description,
+            price,
+            image,
+            category,
+            subcategory,
         });
 
         await product.save();
 
-        res.status(200).send('Producto creado exitosamente');
-
+        res.status(200).send("Producto creado exitosamente");
     } catch (error) {
-        res.status(500).send('Hubo un problema con el post');
+        res.status(500).send("Hubo un problema con el post");
     }
 });
 
-
-router.get('/products', async (req, res) => {
+// ruta para traer todos los productos
+router.get("/products", async (req, res) => {
     const products = await Product.find();
     // aquí se va a escribir el código para obtener los productos de la base de datos
-    res.status(200).json(products) 
-    
+    res.status(200).json(products);
 });
 
+// ruta para modificar la informacion del producto
+router.put("/product/:id", async (req, res) => {
+    const {id} = req.params;
+    const {name, price} = req.body;
 
+    try {
+        const updateProduct = await Product.findByIdAndUpdate(
+            id,
+            {name, price},
+            {new: true}
+        );
+
+        res.status(200).json(updateProduct);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Error al actualizar el producto'});
+    }
+});
 
 module.exports = router;
