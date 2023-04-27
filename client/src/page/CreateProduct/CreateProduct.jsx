1ca8ff05cf0e6913b8b16e85fc3ba97a17b00c7e
tomigarcia <tomigarcia89@gmail.com>
2023-04-26 23:58:34 -0300

    creando estilo a creatProduct

--- client/src/page/CreateProduct/CreateProduct.jsx
+++ client/src/page/CreateProduct/CreateProduct.jsx
@@ -4,17 +4,36 @@ import styles from './CreateProduct.module.css'
 export const CreateProduct = () => {
   return (
     
-    <div className={styles.create_product}> 
-       <h2 className={styles.h2}>Esta seccion es para crear un producto</h2>
-       <form className={styles.form}>
-       <input className={styles.input} type="text" placeholder="Nombre del Producto" />
-        <input className={styles.input2} type="number" placeholder="Precio" />
-        <input className={styles.input3} type="text" placeholder='Categoria'/>
-        <button className={styles.button}>Create</button>
-      </form>
+    <div className={styles.create_container}>
+      <form >
+        
       
+        <div className={styles.create_container_info}>
+
+          <div className={styles.create_container_info_picture}>
+             <input type="file" />
+
+          </div>
+          <div className={styles.create_container_info_inputs}>
+             <input type="text" placeholder='Ingrese el nombre'/>
+             <select name="" id="">
+              <option value="" disabled>Ingrese una categoria</option>
+             </select>
+             <select name="" id="">
+              <option value="" disabled>Ingrese una subcategoria</option>
+             </select>
+             <input type="number" placeholder='Precio'/>
+             <input type="number" placeholder='Stock'/>
+              
+              
+          </div>
+        </div>
+        <div className={styles.create_container_description}>
+            <textarea placeholder='Ingrese la descripcion'></textarea>
+        </div>
+        <button type="submit" className={styles.button}>Crear</button>
+        </form>
     </div>
-      
       
       
     
