7a0f56b0ebb974f8ee4980c18dcac24e9d80895d
lucasponce8 <lucasponce8.lp@gmail.com>
2023-06-14 13:26:05 -0300

    agregando funcionalidades al carrito

--- client/src/components/CounterDetail/CounterDetail.jsx
+++ client/src/components/CounterDetail/CounterDetail.jsx
@@ -1,45 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import styles from './CounterDetail.module.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import SliderCart from '../SliderCart/SliderCart';
+import useCart from '../../hooks/useCart';
 
 
-const CounterDetail = ({product, addToCart, stock, cart}) => {
-    
-
+const CounterDetail = ({ product, addToCart, stock}) => {
+    const {cart} = useCart();
     let mapCartId = cart.map(item => item._id);
-    
-    let filterCart = mapCartId.includes(product._id)
-    
-    // console.log(mapCartId)
-    // console.log(filterCart)
-    
-    
-  return (
-    <div className={styles.counterCart}>
+    let filterCart = mapCartId.includes(product._id);
+    const [carrito, setCarrito] = useState([]);
+    // useEffect(() => {
+    //     const storedCart = JSON.parse(localStorage.getItem('cart')) || [];
+    //     setCarrito(storedCart);
+    //   }, []);
 
-        {
-            !filterCart ?
-            <div
-                className={styles.counterCart_sendCart}
-                onClick={() => addToCart(product)}
-            >
-                <p>Agregar al carrito</p>
-            </div>
-            :
-            <Link to='/cart'>
-                <div
-                    className={styles.counterCart_sendCart}
-                >
-                    <p>Ir al carrito</p>
+    const [showSlider, setShowSlider] = useState(false);
+    const toggleSlider = () => {
+      setShowSlider(!showSlider);
+    };
+  
+    const handleAddToCart = () => {
+      addToCart(product);
+      toggleSlider(); // Abrir el menú del carrito desplegable
+    //   localStorage.setItem('cart', JSON.stringify([...cart, product]));  
+    };
+  
+    return (
+      <div className={styles.counterCart}>
+            {!filterCart ? (
+                <div className={styles.counterCart_sendCart} onClick={handleAddToCart}>
+                    <div onClick={toggleSlider}>
+                        <p>Agregar al carrito</p>
+                    </div>
                 </div>
-            </Link>
-
-        }
-
-    </div>
-  )
-}
-
+            ) : (
+                    <Link to="/cart">
+                        <div className={styles.counterCart_sendCart}>
+                            <p>Ir al carrito</p>
+                        </div>
+                    </Link>
+                )}
+            {
+                showSlider 
+                && 
+                <div 
+                    className={styles.sliderOverlay} 
+                    onClick={toggleSlider}
+                >
+                </div>}
+            {
+                showSlider 
+                && 
+                <SliderCart 
+                    onClose={toggleSlider} 
+                />
+            }
+      </div>
+    );
+};
+  
 export default CounterDetail;
+  
